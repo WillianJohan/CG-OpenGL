1546696c9f9098fc1438ab752e2133bcd60bf974
willianjohan <johan_willian@hotmail.com>
2020-09-26 19:41:11 -0300

    Calculando os vertices de um cubo, aplicando transformações e mostrando cubo na tela

--- UFN_CG/UFN_CG/Forms/Form1.cs
+++ UFN_CG/UFN_CG/Forms/Form1.cs
@@ -13,78 +13,51 @@ namespace UFN_CG
         {
             InitializeComponent();
             
-            //Criando um objeto de transformações 2D
             transform = new Transform2D();
-
-            //Setando a posicao do objeto para um ponto da tela
             transform.Position = new Vector2(200,200);
-            //transform.Scale *= 2;
-            transform.rotate(30);
 
-            //Criando as vértices no ponto central de origem e posicionando na posição inicial da tela
             vertices = new Vector2[]
             {
-                new Vector2(-100,-100) + transform.Position,
-                new Vector2(100,-100)  + transform.Position,
-                new Vector2(100,100)   + transform.Position,
-                new Vector2(-100,100)  + transform.Position
+                new Vector2(-100,-100),
+                new Vector2(100,-100),
+                new Vector2(100,100),
+                new Vector2(-100,100)
             };
          
             this.Size = new Size(1000, 1000);
             canvas.Size = Size;
             paintLines();
-
-            //Console.WriteLine("Criando o Transform.");
-            //Transform2D t = new Transform2D();
-            //t.Position = new Vector2(10, 10);
-            //Console.WriteLine("T::Posicao Inicial: " + t.Position);
-            //
-            //t.translate(10, 5);
-            //Console.WriteLine("T::Transladando: " + new Vector2(10, 5));
-            //Console.WriteLine("T::Posicao final: " + t.Position);
-            //
-            //Console.WriteLine("Vertice no ponto (0,0)");
-            //Console.WriteLine("vertice aplicada transformacao: " +
-            //     t.TransformationMatrix * new Vector3(0, 0, 1));
-
         }
 
-        private void button1_Click(object sender, System.EventArgs e)
+        private void button1_Click(object sender, System.EventArgs e) 
         {
-
-            //transform.translate(0, 50);
-            //transform.rotate(-5);
-            //transform.Scale *=1.01f;
-
-            for (int i = 0; i < vertices.Length; i++)
-            {
-                Vector3 vertice = new Vector3(vertices[i].x, vertices[i].y, 1) - (Vector3)transform.Position;
-                Vector3 newPoint = transform.TransformationMatrix * vertice;
-                vertices[i] = (Vector2)newPoint + transform.Position;
-            }
-            Console.WriteLine(transform.TransformationMatrix*(Vector3)vertices[2]);
+            //transform.translate(50, 0);
+            //transform.Scale *= .5f;
+            transform.rotate(10);
             paintLines();
-            
         }
-
 
         void paintLines()
         {
-            Graphics g = canvas.CreateGraphics();
+            Graphics graphic = canvas.CreateGraphics();
             canvas.Size = Size;
-            g.Clear(Color.White);
+            graphic.Clear(Color.White);
             Brush brush = new SolidBrush(Color.Red);
             Pen p = new Pen(brush, 3);
 
             for (int i = 0; i < vertices.Length; i++)
             {
-                Point ini = new Point((int)vertices[i].x, (int)vertices[i].y);
-                Point end = (i + 1 >= vertices.Length) ? new Point((int)vertices[0].x, (int)vertices[0].y) : new Point((int)vertices[i + 1].x, (int)vertices[i + 1].y);
-                g.DrawLine(p, ini, end);
-            }
+                Vector3 v_Ini = (new Vector3(vertices[i].x, vertices[i].y, 1) - (Vector3)transform.Position) * transform.TransformationMatrix;
+                Vector3 v_End = (((i + 1 >= vertices.Length) ? new Vector3(vertices[0].x, vertices[0].y, 1) : new Vector3(vertices[i+1].x, vertices[i+1].y, 1)) - (Vector3)transform.Position) * transform.TransformationMatrix;
 
+                v_Ini += (Vector3)transform.Position;
+                v_End += (Vector3)transform.Position;
+
+                Point ini = new Point((int)v_Ini.x, (int)v_Ini.y);
+                Point end = new Point((int)v_End.x, (int)v_End.y);
                 
+                graphic.DrawLine(p, ini, end);
+            }
         }
-
     }
 }
