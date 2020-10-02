3f767ccff272d56043b8b7403e37f1171391a8b5
willianjohan <johan_willian@hotmail.com>
2020-10-01 21:59:16 -0300

    Mesh e MeshFilter implementado

---
+++ UFN_CG/UFN_CG/Classes/Mesh/MeshFilter.cs
@@ -0,0 +1,29 @@
+﻿namespace UFN_CG
+{
+    public class MeshFilter
+    {
+        Mesh mesh;
+
+        public Mesh Mesh{ get => mesh; set=> mesh = value; }
+
+        #region Constructors
+
+        public MeshFilter() => Mesh = null;
+
+        public MeshFilter(Mesh mesh) => this.mesh = mesh;
+
+        public MeshFilter(string pathTo_3D_Object) 
+        {
+            // Abre arquivo e extrai as informaçoes de vertices e triangulos.
+            // Monta uma nova Mesh com todos os vertices e triangulos
+        }
+
+        #endregion
+
+        #region Read and create a new mesh 
+
+        // Not implemented yet
+
+        #endregion
+    }
+}
