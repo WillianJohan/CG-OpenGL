57127688200ce5847053f0f6be39eda37691b04c
WillianJohan <johan_willian@hotmail.com>
2020-10-23 12:27:33 -0300

    Projeto em C++ para implementação do OpenGL Criado

---
+++ Cpp_OpenGL/UFN_CG_OpenGL/src/Application.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() 
+{
+	std::cout << "= UFN - Computacao Grafica =======================" << std::endl;
+	std::cout << "= Implementando uma Aplicacao utilizando OpenGL  =" << std::endl;
+	std::cout << "==================================================" << std::endl;
+	std::cin.get();
+}
