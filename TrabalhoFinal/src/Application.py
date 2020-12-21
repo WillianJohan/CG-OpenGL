393cf33e7cb643eb653a4a79ea1c55da35def4bf
WillianJohan <johan_willian@hotmail.com>
2020-12-21 17:06:10 -0300

    Inicializando uma janela

--- TrabalhoFinal/src/Application.py
+++ TrabalhoFinal/src/Application.py
@@ -1,54 +1,11 @@
-class Mimi:
-    def __init__(self, n : int):
-        self.n = n
+from Window import *
+import glfw
 
+window = Window(100,100,"teste")
 
-
-
-
-
-class teste01:
-    def __init__(self, obj : Mimi):
-        self.obj = obj
-
-    def alterarValor(self, n):
-        print("[Teste01] valor original: ", self.obj.n, " Valor Alterado para: ", n)
-        self.obj.n = n
-
-
-
-
-
-
-class teste02:
-    def __init__(self, obj : Mimi):
-        self.obj = obj
-
-    def alterarValor(self, n):
-        print("[Teste02] valor original: ", self.obj.n, " Valor Alterado para: ", n)
-        self.obj.n = n
-
-
-
-
-
-
-
-
-mimi = Mimi(10)
-print("mimi instanciado: ", mimi.n)
-
-instanciaTeste01 = teste01(mimi)
-instanciaTeste02 = teste02(mimi)
-
-instanciaTeste01.alterarValor(1)
-print("Valor do objeto apos o teste: ", mimi.n)
-instanciaTeste02.alterarValor(4)
-print("Valor do objeto apos o teste: ", mimi.n)
-
-instanciaTeste01.alterarValor(10)
-instanciaTeste02.alterarValor(71)
-instanciaTeste01.alterarValor(12)
-
-
-print("Valor do objeto apos o teste: ", mimi.n)
+while not glfw.window_should_close(window.Canvas):
+        
+    if (glfw.PRESS == glfw.get_key(window.Canvas, glfw.KEY_ESCAPE)):
+        glfw.set_window_should_close(window.Canvas, True)
+    
+glfw.terminate()
