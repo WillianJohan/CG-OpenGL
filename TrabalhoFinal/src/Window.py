ff9805b39cbbc2234d37711c2c9a5e86eb970ec7
WillianJohan <johan_willian@hotmail.com>
2020-12-21 17:05:54 -0300

    Update Window.py

--- TrabalhoFinal/src/Window.py
+++ TrabalhoFinal/src/Window.py
@@ -7,27 +7,29 @@ class Window:
         self.WIDTH = WIDTH
         self.HEIGHT = HEIGHT
         self.TITLE_WINDOW = TITLE_WINDOW
-        self.WINDOW = None
+        self.Canvas = None
 
-        self._CreateAndInitGLFW()
+        self._initialize_Glfw()
 
         print("Placa de vídeo: ", OpenGL.GL.glGetString(OpenGL.GL.GL_RENDERER))
         print("Versão do OpenGL: ", OpenGL.GL.glGetString(OpenGL.GL.GL_VERSION))
 
-    def _CreateAndInitGLFW(self):
+
+    def _initialize_Glfw(self):
         # Inicializa GLFW
         glfw.init()
 
         # Criação de uma janela
-        self.WINDOW = glfw.create_window(
+        self.Canvas = glfw.create_window(
             self.WIDTH, self.HEIGHT, self.TITLE_WINDOW, None, None)
-        if not self.WINDOW:
+        if not self.Canvas:
             glfw.terminate()
             exit()
 
-        glfw.set_window_size_callback(self.WINDOW, self._ResizeCallBack)
-        glfw.make_context_current(self.WINDOW)
+        glfw.set_window_size_callback(self.Canvas, self._ResizeCallBack)
+        glfw.make_context_current(self.Canvas)
 
-    def _ResizeCallBack(self, w: int, h: int):
-        self.WIDTH = w
-        self.HEIGHT = h
+
+    def _ResizeCallBack(self, WIDTH: int, HEIGHT: int):
+        self.WIDTH = WIDTH
+        self.HEIGHT = HEIGHT
