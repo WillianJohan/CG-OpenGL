c31955e74846da7ecb36a914454efe4a7e0c25d2
willianjohan <johan_willian@hotmail.com>
2020-12-24 10:49:38 -0300

    Criando a base de execução do projeto

---
+++ TrabalhoFinal/Utilizando CSharp (Testando Implementacao)/Renderer3D/Renderer3D/GameLoop/Game.cs
@@ -0,0 +1,46 @@
+﻿using GLFW;
+
+namespace Renderer3D.GameLoop
+{
+    public abstract class Game
+    {
+        protected int InitialWindowWidth { get; set; }
+        protected int InitialWindowHeight { get; set; }
+        protected string InitialWindowTitle { get; set; }
+
+        protected Game(int initialWindowWidth, int initialWindowHeight, string initialWindowTitle)
+        {
+            this.InitialWindowWidth = initialWindowWidth;
+            this.InitialWindowHeight = initialWindowHeight;
+            this.InitialWindowTitle = initialWindowTitle;
+        }
+
+        public void Run() 
+        {
+            Initialize();
+            Renderer.Display.DisplayManager.CreateWindow(InitialWindowWidth, InitialWindowHeight, InitialWindowTitle);
+            LoadContent();
+
+            while (!Glfw.WindowShouldClose(Renderer.Display.DisplayManager.window))
+            {
+                GameTime.DeltaTime = (float)Glfw.Time - GameTime.TotalElapsedSeconds;
+                GameTime.TotalElapsedSeconds = (float)Glfw.Time;
+
+                Update();
+                
+                Glfw.PollEvents();
+                
+                Render();
+            }
+
+            Renderer.Display.DisplayManager.CloseWindow();
+        }
+
+        protected abstract void Initialize();
+        protected abstract void LoadContent();
+
+
+        protected abstract void Update();
+        protected abstract void Render();
+    }
+}
