6b5882bc39f6d171dcb91772b8d9d57b92164dea
willianjohan <johan_willian@hotmail.com>
2020-12-24 16:14:04 -0300

    Desenhando Quadrado colorido

--- TrabalhoFinal/Utilizando CSharp (Testando Implementacao)/Renderer3D/Renderer3D/TesteGame.cs
+++ TrabalhoFinal/Utilizando CSharp (Testando Implementacao)/Renderer3D/Renderer3D/TesteGame.cs
@@ -6,6 +6,10 @@ namespace Renderer3D
 {
     class TesteGame : GameLoop.Game
     {
+        uint vao;
+        uint vbo;
+        uint shader;
+
         public TesteGame(int initialWindowWidth, int initialWindowHeight, string initialWindowTitle) : base(initialWindowWidth, initialWindowHeight, initialWindowTitle)
         {
             this.InitialWindowWidth = initialWindowWidth;
@@ -17,8 +21,75 @@ namespace Renderer3D
         {
         }
 
-        protected override void LoadContent()
+        protected unsafe override void LoadContent()
         {
+            string vertexShader = @"#version 330 core
+                                    layout (location = 0) in vec2 aPosition;
+                                    layout (location = 1) in vec3 aColor;
+                                    out vec4 vertexColor;
+    
+                                    void main() 
+                                    {
+                                        vertexColor = vec4(aColor.rgb, 1.0);
+                                        gl_Position = vec4(aPosition.xy, 0, 1.0);
+                                    }";
+
+            string fragmentShader = @"#version 330 core
+                                    out vec4 FragColor;
+                                    in vec4 vertexColor;
+
+                                    void main() 
+                                    {
+                                        FragColor = vertexColor;
+                                    }";
+
+            uint vs = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vs, vertexShader);
+            glCompileShader(vs);
+
+            uint fs = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fs, fragmentShader);
+            glCompileShader(fs);
+
+            shader = glCreateProgram();
+            glAttachShader(shader, vs);
+            glAttachShader(shader, fs);
+
+            glLinkProgram(shader);
+
+
+            // Criando VAO e VBO
+            vao = glGenVertexArray();
+            vbo = glGenBuffer();
+
+            glBindVertexArray(vao);
+            glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+
+            float[] vertices =
+            {
+                -0.5f, 0.5f, 1f, 0f, 0f, // top left
+                0.5f, 0.5f, 0f, 1f, 0f,// top right
+                -0.5f, -0.5f, 0f, 0f, 1f, // bottom left
+
+                0.5f, 0.5f, 0f, 1f, 0f,// top right
+                0.5f, -0.5f, 0f, 1f, 1f, // bottom right
+                -0.5f, -0.5f, 0f, 0f, 1f, // bottom left
+            };
+
+            fixed(float* v = &vertices[0])
+            {
+                glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, v, GL_STATIC_DRAW);
+            }
+
+            glVertexAttribPointer(0, 2, GL_FLOAT, false, 5 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(0);
+
+            glVertexAttribPointer(1, 3, GL_FLOAT, false, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+            glEnableVertexAttribArray(1);
+
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            glBindVertexArray(0);
         }
        
         protected override void Update()
@@ -27,8 +98,13 @@ namespace Renderer3D
 
         protected override void Render()
         {
-            glClearColor(MathF.Sin(GameLoop.GameTime.TotalElapsedSeconds), 0, 0, 1);
+            glClearColor(0,0,0,0);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            glUseProgram(shader);
+
+            glBindVertexArray(vao);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
 
             Glfw.SwapBuffers(Renderer.Display.DisplayManager.window);
         }
