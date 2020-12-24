81cc1d5659f96ff88cfeb3d0bec9f664f663bc32
willianjohan <johan_willian@hotmail.com>
2020-12-24 10:50:07 -0300

    Create TesteGame.cs

---
+++ TrabalhoFinal/Utilizando CSharp (Testando Implementacao)/Renderer3D/Renderer3D/TesteGame.cs
@@ -0,0 +1,37 @@
+﻿using static Renderer3D.OpenGL.GL;
+using GLFW;
+using System;
+
+namespace Renderer3D
+{
+    class TesteGame : GameLoop.Game
+    {
+        public TesteGame(int initialWindowWidth, int initialWindowHeight, string initialWindowTitle) : base(initialWindowWidth, initialWindowHeight, initialWindowTitle)
+        {
+            this.InitialWindowWidth = initialWindowWidth;
+            this.InitialWindowHeight = initialWindowHeight;
+            this.InitialWindowTitle = initialWindowTitle;
+        }
+
+        protected override void Initialize()
+        {
+        }
+
+        protected override void LoadContent()
+        {
+        }
+       
+        protected override void Update()
+        {
+        }
+
+        protected override void Render()
+        {
+            glClearColor(MathF.Sin(GameLoop.GameTime.TotalElapsedSeconds), 0, 0, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            Glfw.SwapBuffers(Renderer.Display.DisplayManager.window);
+        }
+
+    }
+}
