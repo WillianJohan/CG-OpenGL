e988cad62803606c03e4de6acb2002eb1069d939
willianjohan <johan_willian@hotmail.com>
2021-01-02 11:37:28 -0300

    Create HelperClass.cs para auxiliar com alguns metodos para debug.

---
+++ TrabalhoFinal/Utilizando CSharp (Testando Implementacao)/Renderer3D/Renderer3D/HelperClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public static class HelperClass
+{
+    public static void PrintAllLines(string[] lines)
+    {
+        foreach(string line in lines) 
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
