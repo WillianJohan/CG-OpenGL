407fab16939076f1a9fd6f9992e634e8988b940b
willianjohan <johan_willian@hotmail.com>
2021-01-03 17:35:13 -0300

    Update Scene.cs

--- TrabalhoFinal/Utilizando CSharp (Testando Implementacao)/Renderer3D/Renderer3D/Scene.cs
+++ TrabalhoFinal/Utilizando CSharp (Testando Implementacao)/Renderer3D/Renderer3D/Scene.cs
@@ -1,7 +1,49 @@
-﻿namespace Renderer3D
+﻿using System.Collections.Generic;
+
+namespace Renderer3D
 {
     public class Scene
     {
-        public VirtualCamera virtualCamera;
+        public string SceneName { get; set; }
+        public static VirtualCamera virtualCamera { get; set; }
+        public List<GraphicObject> Objects { get; set; }
+
+        #region Constructors
+        public Scene()
+        {
+            this.SceneName = "Simple Scene";
+            virtualCamera = new VirtualCamera();
+            this.Objects = new List<GraphicObject>();
+        }
+
+        public Scene(string Name)
+        {
+            this.SceneName = Name;
+            virtualCamera = new VirtualCamera();
+            this.Objects = new List<GraphicObject>();
+        }
+
+        public Scene(string Name, VirtualCamera MainCamera)
+        {
+            this.SceneName = Name;
+            virtualCamera = MainCamera;
+            this.Objects = new List<GraphicObject>();
+        }
+
+        public Scene(string Name, VirtualCamera MainCamera, List<GraphicObject> Objects)
+        {
+            this.SceneName = Name;
+            virtualCamera = MainCamera;
+            this.Objects = Objects;
+        }
+
+        public Scene(string Name, List<GraphicObject> Objects)
+        {
+            this.SceneName = Name;
+            virtualCamera = new VirtualCamera();
+            this.Objects = Objects;
+        }
+        #endregion
+
     }
 }
