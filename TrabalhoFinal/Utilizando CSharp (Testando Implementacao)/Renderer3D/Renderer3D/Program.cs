5714eb78a70664762ba676c46ae8e9b0f3cd8564
willianjohan <johan_willian@hotmail.com>
2021-01-03 17:35:22 -0300

    Update Program.cs

--- TrabalhoFinal/Utilizando CSharp (Testando Implementacao)/Renderer3D/Renderer3D/Program.cs
+++ TrabalhoFinal/Utilizando CSharp (Testando Implementacao)/Renderer3D/Renderer3D/Program.cs
@@ -1,11 +1,34 @@
-﻿namespace Renderer3D
+﻿using System.Numerics;
+
+namespace Renderer3D
 {
     class Program
     {
 
         static void Main(string[] args)
         {
-            Game game = new TesteGame(800, 600, "Teste");
+            //Criando um Objeto 3D
+            GraphicObject objQueGira = new GraphicObject("Objeto Simples");
+            objQueGira.transform.Position = new Vector3(400, 300, -10);
+            objQueGira.transform.Scale = new Vector3(150, 100, 1);
+            objQueGira.Vertices = new float[]
+            {
+                -0.5f, 0.5f, 1f, 0f, 0f, // top left
+                0.5f, 0.5f, 0f, 1f, 0f,// top right
+                -0.5f, -0.5f, 0f, 0f, 1f, // bottom left
+
+                0.5f, 0.5f, 0f, 1f, 0f,// top right
+                0.5f, -0.5f, 0f, 1f, 1f, // bottom right
+                -0.5f, -0.5f, 0f, 0f, 1f, // bottom left
+            };
+
+            //Criando a cena
+            Scene cenaTeste = new Scene("Cena Teste");
+            Scene.virtualCamera.IsOrtographic = true;
+            cenaTeste.Objects.Add(objQueGira);
+
+            //Iniciando o Game
+            Game game = new TesteGame(800, 600, "Teste", cenaTeste);
             game.Run();
         }
 
