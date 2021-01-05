b0d5ee2998678df727028b27f302eb8016b3f961
willianjohan <johan_willian@hotmail.com>
2021-01-05 18:30:54 -0300

    Classe estática para Debug Criada

---
+++ TrabalhoFinal/Utilizando CSharp (Testando Implementacao)/Renderer3D/Renderer3D/Debug/Debug.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Renderer3D
+{
+    public static class Debug
+    {
+        public static void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static void WarningLog(string text)
+        {
+            Console.WriteLine("[Warning!] " + text);
+        }
+
+        public static void ErrorLog(string text)
+        {
+            Console.WriteLine("[ERROR!] " + text);
+        }
+
+    }
+}
