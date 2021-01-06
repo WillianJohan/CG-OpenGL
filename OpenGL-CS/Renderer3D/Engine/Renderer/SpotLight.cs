d357a1a4fc537dee18122cd10f3406533043ae53
willianjohan <johan_willian@hotmail.com>
2021-01-06 14:10:20 -0300

    Create SpotLight.cs

---
+++ OpenGL-CS/Renderer3D/Engine/Renderer/SpotLight.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+namespace RendererEngine
+{
+    public class SpotLight
+    {
+        public Transform transform;
+        public Vector3 La { get; private set; }
+        public Vector3 Ld { get; private set; }
+        public Vector3 Ls { get; private set; }
+
+        public SpotLight()
+        {
+            transform = new Transform();
+            setLA(0.2f);
+            setLD(0.7f);
+            setLS(1.0f);
+        }
+
+        public SpotLight(Transform transform, Vector3 la, Vector3 ld, Vector3 ls)
+        {
+            this.transform = transform;
+            La = la;
+            Ld = ld;
+            Ls = ls;
+        }
+
+        public SpotLight(Vector3 la, Vector3 ld, Vector3 ls)
+        {
+            transform = new Transform();
+            La = la;
+            Ld = ld;
+            Ls = ls;
+        }
+
+        public void setLA(float LA) => La = new Vector3(LA);
+        public void setLD(float LD) => Ld = new Vector3(LD);
+        public void setLS(float LS) => Ls = new Vector3(LS);
+
+    }
+}
