6f009d510ad3b07e3037ac8810750530d9292494
willianjohan <johan_willian@hotmail.com>
2021-01-06 14:10:10 -0300

    Update Program.cs

--- OpenGL-CS/Renderer3D/Program.cs
+++ OpenGL-CS/Renderer3D/Program.cs
@@ -7,7 +7,110 @@ namespace RendererEngine
 
         static void Main(string[] args)
         {
-            Runtime game = new Game(800, 600, "Teste", new Scene());
+
+			float[] objVertex = new float[]
+			{
+				//face frontal
+				 0.5f,  0.5f, 0.5f,
+				 0.5f, -0.5f, 0.5f,
+				-0.5f, -0.5f, 0.5f,
+				-0.5f,  0.5f, 0.5f,
+				 0.5f,  0.5f, 0.5f,
+				-0.5f, -0.5f, 0.5f,
+				
+				//face trazeira
+				 0.5f,  0.5f, -0.5f,
+				 0.5f, -0.5f, -0.5f,
+				-0.5f, -0.5f, -0.5f,
+				-0.5f,  0.5f, -0.5f,
+				 0.5f,  0.5f, -0.5f,
+				-0.5f, -0.5f, -0.5f,
+				
+				//face esquerda
+				-0.5f, -0.5f,  0.5f,
+				-0.5f,  0.5f,  0.5f,
+				-0.5f, -0.5f, -0.5f,
+				-0.5f, -0.5f, -0.5f,
+				-0.5f,  0.5f, -0.5f,
+				-0.5f,  0.5f,  0.5f,
+				
+				//face direita
+				0.5f, -0.5f,  0.5f,
+				0.5f,  0.5f,  0.5f,
+				0.5f, -0.5f, -0.5f,
+				0.5f, -0.5f, -0.5f,
+				0.5f,  0.5f, -0.5f,
+				0.5f,  0.5f,  0.5f,
+				
+				//face baixo
+				-0.5f, -0.5f,  0.5f,
+				 0.5f, -0.5f,  0.5f,
+				 0.5f, -0.5f, -0.5f,
+				 0.5f, -0.5f, -0.5f,
+				-0.5f, -0.5f, -0.5f,
+				-0.5f, -0.5f,  0.5f,
+				
+				//face cima
+				-0.5f, 0.5f,  0.5f,
+				 0.5f, 0.5f,  0.5f,
+				 0.5f, 0.5f, -0.5f,
+				 0.5f, 0.5f, -0.5f,
+				-0.5f, 0.5f, -0.5f,
+				-0.5f, 0.5f,  0.5f
+			};
+			float[] objNormals = new float[]
+			{
+				//face frontal
+				0.0f, 0.0f, 1.0f,
+				0.0f, 0.0f, 1.0f,
+				0.0f, 0.0f, 1.0f,
+				0.0f, 0.0f, 1.0f,
+				0.0f, 0.0f, 1.0f,
+				0.0f, 0.0f, 1.0f,
+				//face trazeira
+				0.0f, 0.0f, -1.0f,
+				0.0f, 0.0f, -1.0f,
+				0.0f, 0.0f, -1.0f,
+				0.0f, 0.0f, -1.0f,
+				0.0f, 0.0f, -1.0f,
+				0.0f, 0.0f, -1.0f,
+				//face esquerda
+				-1.0f, 0.0f, 0.0f,
+				-1.0f, 0.0f, 0.0f,
+				-1.0f, 0.0f, 0.0f,
+				-1.0f, 0.0f, 0.0f,
+				-1.0f, 0.0f, 0.0f,
+				-1.0f, 0.0f, 0.0f,          
+				//face direita
+				1.0f, 0.0f, 0.0f,
+				1.0f, 0.0f, 0.0f,
+				1.0f, 0.0f, 0.0f,
+				1.0f, 0.0f, 0.0f,
+				1.0f, 0.0f, 0.0f,
+				1.0f, 0.0f, 0.0f,
+				//face inferior
+				0.0f, -1.0f, 0.0f,
+				0.0f, -1.0f, 0.0f,
+				0.0f, -1.0f, 0.0f,
+				0.0f, -1.0f, 0.0f,
+				0.0f, -1.0f, 0.0f,
+				0.0f, -1.0f, 0.0f,
+				//face superior
+				0.0f, 1.0f, 0.0f,
+				0.0f, 1.0f, 0.0f,
+				0.0f, 1.0f, 0.0f,
+				0.0f, 1.0f, 0.0f,
+				0.0f, 1.0f, 0.0f,
+				0.0f, 1.0f, 0.0f
+			};
+			GameObject simpleObject = new GameObject("Simple Object", new Transform(), new Mesh(objVertex, objNormals));
+
+			Scene simpleScene = new Scene("SimpleScene");
+			simpleScene.Objects.Add(simpleObject);
+			simpleScene.spotLight.transform.Position.X = -2f;
+			simpleScene.virtualCamera.Position.Z = 10f;
+
+            Runtime game = new Game(800, 600, simpleScene.SceneName, simpleScene);
             game.Run();
         }
 
