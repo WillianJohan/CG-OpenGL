6fe36413fa45556ce77ee01ec0e9096f12f1f59c
willianjohan <johan_willian@hotmail.com>
2021-01-06 14:10:35 -0300

    Update GameObject.cs

--- OpenGL-CS/Renderer3D/Engine/Components/GraphicObject/GameObject.cs
+++ OpenGL-CS/Renderer3D/Engine/Components/GraphicObject/GameObject.cs
@@ -12,12 +12,18 @@ namespace RendererEngine
         {
             this.Name = "No Name Object";
             this.transform = new Transform();
+            this.mesh = new Mesh();
+            this.material = new Material();
+            this.shader = new Shader();
         }
 
         public GameObject(string Name)
         {
             this.Name = Name;
             this.transform = new Transform();
+            this.mesh = new Mesh();
+            this.material = new Material();
+            this.shader = new Shader();
         }
 
         public GameObject(string Name, Vector3 Position)
@@ -25,7 +31,9 @@ namespace RendererEngine
             this.Name = Name;
             this.transform = new Transform();
             this.transform.Position = Position;
-
+            this.mesh = new Mesh();
+            this.material = new Material();
+            this.shader = new Shader();
         }
 
         public GameObject(string Name, Vector3 Position, Vector3 Rotation, Vector3 Scale)
@@ -35,12 +43,18 @@ namespace RendererEngine
             this.transform.Position = Position;
             this.transform.Rotation = Rotation;
             this.transform.Scale = Scale;
+            this.mesh = new Mesh();
+            this.material = new Material();
+            this.shader = new Shader();
         }
 
         public GameObject(string Name, Transform transform)
         {
             this.Name = Name;
             this.transform = transform;
+            this.mesh = new Mesh();
+            this.material = new Material();
+            this.shader = new Shader();
         }
 
         public GameObject(string Name, Transform transform, Mesh mesh)
@@ -48,6 +62,17 @@ namespace RendererEngine
             this.Name = Name;
             this.transform = transform;
             this.mesh = mesh;
+            this.material = new Material();
+            this.shader = new Shader();
+        }
+
+        public GameObject(string Name, Transform transform, Mesh mesh, Material material)
+        {
+            this.Name = Name;
+            this.transform = transform;
+            this.mesh = mesh;
+            this.material = material;
+            this.shader = new Shader();
         }
 
         #endregion
