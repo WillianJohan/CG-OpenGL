0bb9e4f2642e4e76c7434507e7975f2fc9e1189c
willianjohan <johan_willian@hotmail.com>
2021-01-06 14:09:28 -0300

    Update Renderable.cs

--- OpenGL-CS/Renderer3D/Engine/Components/GraphicObject/Renderable.cs
+++ OpenGL-CS/Renderer3D/Engine/Components/GraphicObject/Renderable.cs
@@ -7,25 +7,12 @@ namespace RendererEngine
         public Transform transform  { get; set; }
         public Shader shader        { get; protected set; }
         public Mesh mesh            { get; protected set; }
+        public Material material    { get; protected set; }
 
         public unsafe void Load()
         {
             mesh.Load();
             shader.Load();
-        }
-
-        public void Render()
-        {
-            // Apply Transformation in the object
-
-            { //Verificar os calculos pelo codigo do professor
-                transform.Rotation.Z = System.MathF.Sin(Time.TotalElapsedSeconds) * System.MathF.PI * 1f;
-                shader.SetMatrix4x4("model", transform.TransformationMatrix);
-                shader.Use();
-                shader.SetMatrix4x4("projection", Scene.virtualCamera.ProjectionMatrix);
-            }
-
-            mesh.Render();
         }
 
         public void Dispose()
