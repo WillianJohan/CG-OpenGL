0ede7d641770c6291811dda9a4ed4976573d7762
willianjohan <johan_willian@hotmail.com>
2021-01-06 14:10:28 -0300

    Create Material.cs

---
+++ OpenGL-CS/Renderer3D/Engine/Renderer/GraphicElements/Material.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+
+namespace RendererEngine
+{
+    public class Material
+    {
+        public Vector3 AmbientReflection {get; private set;}
+        public Vector3 DiffuseReflection {get; private set;}
+        public Vector3 Specularreflection {get; private set;}
+        public float especular_exp { get; set; }
+
+        public Material(Vector3 ambientReflection, Vector3 diffuseReflection, Vector3 specularreflection)
+        {
+            AmbientReflection = ambientReflection;
+            DiffuseReflection = diffuseReflection;
+            Specularreflection = specularreflection;
+        }
+
+        public Material()
+        {
+            setAmbientReflection(0.2f);
+            setDiffuseReflection(0.5f);
+            setSpecularreflection(1.0f);
+            especular_exp = 32;
+        }
+
+        public void setAmbientReflection(float value) => AmbientReflection  = new Vector3(value);
+        public void setDiffuseReflection(float value) => DiffuseReflection  = new Vector3(value);
+        public void setSpecularreflection(float value) => Specularreflection = new Vector3(value);
+
+
+        public void setAmbientReflection(Vector3 value) => AmbientReflection = value;
+        public void setDiffuseReflection(Vector3 value) => DiffuseReflection = value;
+        public void setSpecularreflection(Vector3 value) => Specularreflection = value;
+
+    }
+}
