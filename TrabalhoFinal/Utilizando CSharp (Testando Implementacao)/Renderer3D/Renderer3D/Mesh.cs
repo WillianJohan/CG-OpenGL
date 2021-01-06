9c19d049a1036fa04e34fa6b658cf33de5f90e05
willianjohan <johan_willian@hotmail.com>
2021-01-05 22:08:15 -0300

    Normals implementado em Mesh.cs

--- TrabalhoFinal/Utilizando CSharp (Testando Implementacao)/Renderer3D/Renderer3D/Mesh.cs
+++ TrabalhoFinal/Utilizando CSharp (Testando Implementacao)/Renderer3D/Renderer3D/Mesh.cs
@@ -5,18 +5,22 @@ namespace RendererEngine
     public class Mesh : System.IDisposable
     {
         public float[] Vertices     { get; private set; }
+        public float[] Normals      { get; private set; }
         public uint VAO             { get; private set; }
-        public uint VBO             { get; private set; }
+        public uint VBO_Vertices    { get; private set; }
+        public uint VBO_Normals     { get; private set; }
 
         #region Constructors
         public Mesh()
         {
             Vertices = new float[0];
+            Normals = new float[0];
         }
 
-        public Mesh(float[] Vertices)
+        public Mesh(float[] Vertices, float[] Normals)
         {
             this.Vertices = Vertices;
+            this.Normals = Normals;
         }
         #endregion
 
@@ -27,16 +31,26 @@ namespace RendererEngine
             glBindVertexArray(VAO);
 
 
-            // Cria o VBO =========================================================
-            VBO = glGenBuffer();
-            glBindBuffer(GL_ARRAY_BUFFER, VBO); //Gera o buffer do ID especificado
+            // Cria o VBO dos Vertices ============================================
+            VBO_Vertices = glGenBuffer();
+            glBindBuffer(GL_ARRAY_BUFFER, VBO_Vertices); //Gera o buffer do ID especificado
 
             fixed (float* verticesPointer = &Vertices[0])
                 glBufferData(GL_ARRAY_BUFFER, sizeof(float) * Vertices.Length, verticesPointer, GL_STATIC_DRAW);
 
-            glVertexAttribPointer(0, 3, GL_FLOAT, true, 0, (void*)0);
             glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0, 3, GL_FLOAT, true, 0, (void*)0);
 
+
+            // Cria o VBO das Normais ============================================
+            VBO_Normals = glGenBuffer();
+            glBindBuffer(GL_ARRAY_BUFFER, VBO_Normals); //Gera o buffer do ID especificado
+
+            fixed (float* normalsPointer = &Normals[0])
+                glBufferData(GL_ARRAY_BUFFER, sizeof(float) * Normals.Length, normalsPointer, GL_STATIC_DRAW);
+
+            glEnableVertexAttribArray(1);
+            glVertexAttribPointer(1, 3, GL_FLOAT, true, 0, (void*)0);
         }
 
         public void Render()
@@ -48,7 +62,7 @@ namespace RendererEngine
         // IDisposable interface
         public void Dispose()
         {
-            glDeleteBuffer(VBO);
+            glDeleteBuffer(VBO_Vertices);
             glDeleteVertexArray(VAO);
         }
     }
